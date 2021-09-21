acd69e13c72637d2b00670332b40a950c5c6391f
Aleksandra-buna <borabora0604@gmail.com>
2021-09-21 13:17:22 +0300

    Adding information about the uploaded file

--- downloader.py
+++ downloader.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-
 from pytube import YouTube
 from tkinter import filedialog
 
@@ -19,28 +18,28 @@ title_link = tk.Label(master=window, text='URL', bg='#b3d6d5', fg='#2d29a0',
                       font=('Cambria', 16, 'bold'))
 title_link.pack(padx=30, pady=38, anchor='nw')
 
-url = tk.StringVar()
-link = tk.Entry(master=window, bg='white', fg='#2d29a0', font=('Cambria', 16), width=38, textvariable=url)
+str_url = tk.StringVar()
+link = tk.Entry(master=window, bg='white', fg='#2d29a0', font=('Cambria', 16), width=38, textvariable=str_url)
 link.place(x=92, y=104)
 
-error_link = tk.Label(master=window, bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 12, 'bold'))
-error_link.place(x=300, y=142)
+error_link = tk.Label(master=window, bg='#b3d6d5', fg='red', font=('Cambria', 12, 'bold'))
+error_link.place(x=30, y=142)
 
-# creating window for input to path
+# creating a window for input to path
 title_path = tk.Label(master=window, text='Path', bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 14, 'bold'))
 title_path.pack(padx=30, pady=18, anchor='nw')
 
-path = tk.Entry(master=window, bg='white', fg='#2d29a0', font=('Cambria', 16), width=38)
+path = tk.Label(master=window, bg='white', fg='#2d29a0', font=('Cambria', 16), width=38)
 path.place(x=92, y=188)
 
-path_error = tk.Label(master=window, bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 12, 'bold'))
-path_error.place(x=300, y=260)
+path_error = tk.Label(master=window, bg='#b3d6d5', fg='red', font=('Cambria', 12, 'bold'))
+path_error.place(x=30, y=260)
 
 select_button = tk.Button(master=window, text='select', bg='#2d29a0', fg='#b3d6d5', font=('Cambria', 11, 'bold'),
-                          activeforeground='#2d29a0', activebackground='#b3d6d5')
+                          activeforeground='#2d29a0', activebackground='#b3d6d5', command=choice_path)
 select_button.place(x=300, y=230)
 
-# creating window "Type Video"
+# creating a window "Type Video"
 title_type = tk.Label(master=window, text='Type', bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 14, 'bold'))
 title_type.pack(padx=98, pady=70, anchor='nw')
 
@@ -49,30 +48,40 @@ types_entry = ['High Quality', 'Low Quality', 'Audio']
 # changing appearance of the selected component
 combostyle = ttk.Style()
 combostyle.theme_create('combostyle', settings={'TCombobox': {'configure':
-                                                               {'foreground': '#2d29a0',
-                                                                'selectbackground': 'white',
-                                                                'selectforeground': '#2d29a0'}}})
+                                                                  {'foreground': '#2d29a0',
+                                                                   'selectbackground': 'white',
+                                                                   'selectforeground': '#2d29a0'}}})
 combostyle.theme_use('combostyle')
 
-record_window = ttk.Combobox(master=window, values=types_entry, font=('Cambria', 14, 'bold'),
-                             width=27, state='readonly')
-record_window.current(0)
-record_window.place(x=159, y=308)
+type_record = ttk.Combobox(master=window, values=types_entry, font=('Cambria', 14, 'bold'),
+                           width=27, state='readonly')
+type_record.current(0)
+type_record.place(x=159, y=308)
 # changing appearance of the list of components
-record_window.master.option_add('*TCombobox*Listbox.foreground', '#2d29a0')
-record_window.master.option_add('*TCombobox*Listbox.selectForeground', '#2d29a0')
-record_window.master.option_add('*TCombobox*Listbox.selectBackground', '#b3d6d5')
-record_window.master.option_add('*TCombobox*Listbox.font', ('Cambria', 12))
+type_record.master.option_add('*TCombobox*Listbox.foreground', '#2d29a0')
+type_record.master.option_add('*TCombobox*Listbox.selectForeground', '#2d29a0')
+type_record.master.option_add('*TCombobox*Listbox.selectBackground', '#b3d6d5')
+type_record.master.option_add('*TCombobox*Listbox.font', ('Cambria', 12))
 
-# creating button "Download"
-# style_download = ttk.Style()
-# style_download.configure('DD.TButton', background='#2d29a0', foreground='#b3d6d5',
-#                          font=('Cambria', 24, 'bold'))
-
+# creating a button "Download"
 download_button = tk.Button(master=window, text='DOWNLOAD', background='#2d29a0', foreground='#b3d6d5',
                             font=('Cambria', 22, 'bold'), activeforeground='#2d29a0', activebackground='#b3d6d5', width=20)
 download_button.pack(pady=20)
 
+download_info = tk.Label(master=window,
+                         text='You have to fill all windows and click the "Download" button, then will begin '
+                              'download.', bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 11, 'bold'))
+download_info.place(x=30, y=500)
+
+# creating a window of information about the downloaded file
+download_name = tk.Label(master=window, bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 12, 'bold'))
+download_name.place(x=30, y=550)
+
+download_size = tk.Label(master=window, bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 12, 'bold'))
+download_size.place(x=30, y=600)
+
+download_path = tk.Label(master=window, bg='#b3d6d5', fg='#2d29a0', font=('Cambria', 12, 'bold'))
+download_path.place(x=30, y=650)
 
 if __name__ == '__main__':
     window.mainloop()
